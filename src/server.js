956c4b1469a2dc5034bc3bee70deabf48ccae5b3
Patrick Wieland <myrkgard@gmail.com>
2022-04-02 17:29:54 +0200

    Removed Babel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import express from "express";
+const express = require("express");
 
 // Constants
 const PORT = 8080;
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("API");
+  res.send("CAPI");
 });
 
 app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
